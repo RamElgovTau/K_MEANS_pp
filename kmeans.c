4faa02465e3369ba326e4446b852a120ad106e0f
MohammadDaghash <80357261+MohammadDaghash@users.noreply.github.com>
2022-05-31 09:58:27 +0300

    Update kmeans.c

--- kmeans.c
+++ kmeans.c
@@ -123,61 +123,122 @@ static int run_kmeans(double** data_points, double** centroids_pp, int n, int d,
 }
 
 /*-----------------CAPI------------------*/
-static PyObject* fit(PyObject *self, PyObject *args) {
-    int k, max_iter, n, d;
+static PyObject *fit(int k, int maxIter, int n, int d, double epsilon, PyObject *data_points.tolist(), PyObject *centroids.tolist()) {
+    /* ~change the inputs from python to c:~ */
+    double *currcentroids, **points;
+    PyObject * line, *member, *PYresult;
+    int i, j;
+    PyObject *finalmergeList = data_points.tolist();
+    PyObject *kcentroids = centroids.tolist();
+    currcentroids = (double *) malloc(d * k * sizeof(double));
+    points = (double **) malloc(n * sizeof(double *));
+    for (i = 0; i < k; i++) {
+        for (j = 0; j < d; j++) {
+            member = PyList_GetItem(kcentroids, i * d + j);
+            currcentroids[i * d + j] = PyFloat_AsDouble(member);
+        }
+    }
+    for (i = 0; i < n; i++) {
+        double *point = malloc(d * sizeof(double));
+        line = PyList_GetItem(finalmergeList, i);
+        for (j = 0; j < d; j++) {
+            member = PyList_GetItem(line, j);
+            point[j] = PyFloat_AsDouble(member);
+        }
+        points[i] = point;
+    }
+    k_meansM(points, currcentroids, epsilon, k, d, n, maxIter);
+
+    PYresult = PyList_New(k);
+    for (i = 0; i < k; i++) {
+        PyObject * PYcentroid = PyList_New(d);
+        for (j = 0; j < d; j++) {
+            PyObject * mem = PyFloat_FromDouble(currcentroids[i * d + j]);
+            PyList_SET_ITEM(PYcentroid, j, mem);
+        }
+        PyList_SET_ITEM(PYresult, i, PYcentroid);
+    }
+
+    for (i = 0; i < n; i++) {
+        free(points[i]);
+    }
+    free(points);
+    free(currcentroids);
+    return PYresult;
+}
+
+static PyObject *k_meansM(PyObject *self, PyObject *args) {
+    int k, maxIter, n, d;
     double epsilon;
-    PyObject *data_points_from_python, *centroids_from_python;
-    double** data_points;
-    double** centroids_pp;
+    PyObject * finalmergeList, *kcentroids;
     /*
      * In the C/Python API, a NULL value is never valid for a PyObject*, so it's used to signal that an error has happened.
      */
-    if (!PyArg_ParseTuple(args, "iiiidOO", &k, &max_iter, &n, &d, &epsilon,
-     &data_points_from_python, &centroids_from_python)) {
+    if (!PyArg_ParseTuple(args, "iiiidOO", &k, &maxIter, &n, &d, &epsilon, &finalmergeList, &kcentroids)) {
         return NULL;
     }
-    data_points = read_2d_array_from_python(n, d, data_points_from_python);
-    centroids_pp = read_2d_array_from_python(k, d, centroids_from_python);
-    if (run_kmeans(data_points, centroids_pp, n, d, k, max_iter, epsilon) != 0) {
-      free_mat(data_points, n);
-      free_mat(centroids_pp, k);
-      return NULL;
-    } else {
-      free_mat(data_points, n);
-      free_mat(centroids_pp, k);
-      return pass_2d_array_to_python(k, d, centroids_pp);
-    }
-}
-static double** read_2d_array_from_python(int rows, int cols, PyObject *py_list) {
-  int i, j;
-  PyObject *row_i, *data;
-  double **arr;
-  arr = calloc(rows, sizeof(double*));
-  for (i = 0; i < rows; ++i) {
-    arr[i] = calloc(cols, sizeof(double));
-    row_i = PyList_GetItem(py_list, i);
-    for (j = 0; j < cols; ++j) {
-      data = PyList_GetItem(row_i, j);
-      arr[i][j] = PyFloat_AsDouble(data);
-    }
-  }
-  return arr;
+    return fit(k, maxIter, n, d, epsilon, finalmergeList, kcentroids);
 }
 
-static PyObject* pass_2d_array_to_python(int rows, int cols, double **arr) {
-  int i, j;
-  PyObject *row_i, *arr_to_python, *data;
-  arr_to_python = PyList_New(rows);
-  for (i = 0; i < rows; ++i) {
-    row_i = PyList_New(cols);
-    for (j = 0; j < cols; ++j) {
-      data = PyFloat_FromDouble(arr[i][j]);
-      PyList_SET_ITEM(row_i, j, data);
-    }
-    PyList_SET_ITEM(arr_to_python, i, row_i);
-  }
-  return arr_to_python;
-}
+
+
+
+
+// static PyObject* fit(PyObject *self, PyObject *args) {
+//     int k, max_iter, n, d;
+//     double epsilon;
+//     PyObject *data_points_from_python, *centroids_from_python;
+//     double** data_points;
+//     double** centroids_pp;
+//     /*
+//      * In the C/Python API, a NULL value is never valid for a PyObject*, so it's used to signal that an error has happened.
+//      */
+//     if (!PyArg_ParseTuple(args, "iiiidOO", &k, &max_iter, &n, &d, &epsilon,
+//      &data_points_from_python, &centroids_from_python)) {
+//         return NULL;
+//     }
+//     data_points = read_2d_array_from_python(n, d, data_points_from_python);
+//     centroids_pp = read_2d_array_from_python(k, d, centroids_from_python);
+//     if (run_kmeans(data_points, centroids_pp, n, d, k, max_iter, epsilon) != 0) {
+//       free_mat(data_points, n);
+//       free_mat(centroids_pp, k);
+//       return NULL;
+//     } else {
+//       free_mat(data_points, n);
+//       free_mat(centroids_pp, k);
+//       return pass_2d_array_to_python(k, d, centroids_pp);
+//     }
+// }
+// static double** read_2d_array_from_python(int rows, int cols, PyObject *py_list) {
+//   int i, j;
+//   PyObject *row_i, *data;
+//   double **arr;
+//   arr = calloc(rows, sizeof(double*));
+//   for (i = 0; i < rows; ++i) {
+//     arr[i] = calloc(cols, sizeof(double));
+//     row_i = PyList_GetItem(py_list, i);
+//     for (j = 0; j < cols; ++j) {
+//       data = PyList_GetItem(row_i, j);
+//       arr[i][j] = PyFloat_AsDouble(data);
+//     }
+//   }
+//   return arr;
+// }
+
+// static PyObject* pass_2d_array_to_python(int rows, int cols, double **arr) {
+//   int i, j;
+//   PyObject *row_i, *arr_to_python, *data;
+//   arr_to_python = PyList_New(rows);
+//   for (i = 0; i < rows; ++i) {
+//     row_i = PyList_New(cols);
+//     for (j = 0; j < cols; ++j) {
+//       data = PyFloat_FromDouble(arr[i][j]);
+//       PyList_SET_ITEM(row_i, j, data);
+//     }
+//     PyList_SET_ITEM(arr_to_python, i, row_i);
+//   }
+//   return arr_to_python;
+// }
 
 /*
  * This array tells Python what methods this module has.
